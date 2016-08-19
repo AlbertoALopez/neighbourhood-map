64e5e1913860fec21d2060da60ae1150fcd15347
Alberto Lopez <albertolopez19@gmail.com>
2016-08-19 00:28:51 -0400

    Add description to locations, remove lat/lon from list

--- src/js/viewmodel/viewmodel.js
+++ src/js/viewmodel/viewmodel.js
@@ -1,41 +1,140 @@
 import ko from 'knockout';
-// import places from '../models/places.js';
 
-const mapLocation = (jsonObj) => {
-    const self = this;
-    self.name = dataObj.name;
+// Initialize google map
+const mapCenter = new google.maps.LatLng(43.7181557,-79.5181427);
+
+const mapOptions = {
+    disableDefaultUI: true,
+    center: mapCenter,
+    zoom: 10,
 };
 
+const map = new google.maps.Map(document.getElementById('map'),
+	mapOptions);
+
+// Map location object
+function MapLocation(jsonObj) {
+    const self = this;
+    self.name = jsonObj.dot_buildingName;
+    self.latitude = parseFloat(jsonObj.dot_Address.dot_Latitude);
+    self.longitude = parseFloat(jsonObj.dot_Address.dot_Longitude);
+    self.description = jsonObj.dot_FullDescription[0];
+
+    self.mapMarker = new google.maps.Marker({
+        position: {
+            lat: self.latitude,
+            lng: self.longitude
+        },
+        map: map,
+        title: self.name
+    });
+
+    self.infoWindow = new google.maps.InfoWindow();
+
+    self.showInfoWindow = () => {
+        if (!self.infoWindow.getContent()) {
+            self.infoWindow.setContent('Loading content...');
+            const content = `
+            <h3 class="info-title">${self.name}</h3>
+            <p class="info-description">${self.description}</p>`;
+            self.infoWindow.setContent(content);
+        }
+
+        self.infoWindow.open(map, self.mapMarker);
+    };
+
+    self.activate = () => {
+        if (MapLocation.prototype.active) {
+            if (MapLocation.prototype.active !== self) {
+                MapLocation.prototype.active.deactivate();
+            }
+        }
+
+        self.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+        self.showInfoWindow();
+
+        MapLocation.prototype.active = self;
+    };
+
+    self.deactivate = () => {
+        self.mapMarker.setAnimation(null);
+        self.infoWindow.close();
+
+        MapLocation.prototype.active = null;
+    };
+
+    self.focus = () => {
+        map.panTo({
+            lat: self.latitude,
+            lng: self.longitude
+        });
+        self.activate();
+    };
+
+    self.mapMarkerClickHandler = () => {
+        if (MapLocation.prototype.active === self) {
+            self.deactivate();
+        }
+        else {
+            self.activate();
+        }
+    };
+
+    self.infoWindowCloseClickHandler = () => {
+        self.deactivate();
+    };
+
+    self.mapMarker.addListener('click', self.mapMarkerClickHandler);
+    self.infoWindow.addListener('closeclick', self.infoWindowCloseClickHandler);
+}
+
+// Knockout view model
 export default function appViewModel() {
     const self = this;
-    self.stations = ko.observableArray([]);
-    self.places = [
-        {
-            name: 'Bovine Sex Club', lat: 43.6476403, lon: -79.4049726
-        },
-        {
-            name: 'TIFF Bell Lightbox', lat: 43.6467232, lon: -79.3925429
-        },
-        {
-            name: 'Kensington Market', lat: 43.6547861, lon: -79.4023693
-        },
-        {
-            name: 'Sneaky Dee\'s', lat: 43.656333, lon: -79.4096757
-        },
-        {
-            name: 'Pomegranate Restaurant', lat: 43.656333, lon: -79.4096757
-        },
-    ];
+    self.locations = ko.observableArray([]);
+    self.filter = ko.observable('');
+    self.loadingMsg = ko.observable('Loading locations...');
+    self.isVisible = ko.observable(true);
+
+    // Function that toggles list view visibility
+    self.toggleVisibility = () => {
+        self.isVisible(!self.isVisible());
+    };
+
+    // Focus on clicked location
+    self.clickHandler = (location) => {
+        location.focus();
+    };
 
     const jsonUrl = 'http://wx.toronto.ca/inter/culture/doorsopen.nsf/DoorsOpenBuildingJSON.xsp';
-
     $.ajax({
         url: jsonUrl,
         dataType: 'jsonp',
         jsonpCallback: 'jsonBuildingsCallBack',
-        type: 'GET',
-        success: function(data) {
-            console.log(data);
-        }
+        type: 'GET'
+    }).done((data) => {
+        const locations = [];
+        let location = null;
+        const bounds = new google.maps.LatLngBounds();
+
+        data.forEach((object) => {
+            location = new MapLocation(object);
+            locations.push(location);
+            console.log(location.description);
+
+            // Extend map bounds
+            bounds.extend(location.mapMarker.position);
+        });
+
+        // Update observableArray
+        self.locations(locations);
+
+        // Resize map to fit all locations
+        map.fitBounds(bounds);
+
+        self.loadingMsg(null);
+    }).fail(() => {
+        self.loadingMsg('Cannot load data');
+        console.log('ERROR: Cannot load location data');
     });
 }
